8591b2c36da26a2c31336db42e1230b4899974be
AWS ParallelCluster user <ec2-user@ip-10-0-11-81.ap-southeast-1.compute.internal>
2024-09-07 15:10:38 +0000

    git commit error resolving

--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@3.0.51.162 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED"
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@3.0.51.162 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED -n MyCluster02; ~/update_snapshot.sh data 2 MyCluster02"
 
