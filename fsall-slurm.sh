a668d85c9047260cd6d798fefabc812f2fe2035f
AWS ParallelCluster user <ec2-user@ip-10-0-7-193.ap-southeast-1.compute.internal>
2024-09-16 14:59:54 +0000

    edit freq fsall

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -14,14 +14,11 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-import time; \
-pyh.RPLLFP(saveLevel=1); \
-print(time.localtime());" \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save();"
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:209479287710:awsnotify --message "FSJobDone"
 
